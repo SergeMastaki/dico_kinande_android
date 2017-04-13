adf92a27460339e87786b63d0499b77e2a227c88
Serge Mastaki <serge.mastaki@gmail.com>
2017-04-13 22:42:04 +0200

    Ajout de la sélection de la langue de traduction

--- dicokinande/app/build/gen/io/github/sergemastaki/dicokinande/R.java
+++ dicokinande/app/build/gen/io/github/sergemastaki/dicokinande/R.java
@@ -8,10 +8,16 @@
 package io.github.sergemastaki.dicokinande;
 
 public final class R {
+    public static final class array {
+        public static final int trans_lang=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int mainSpinner=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -20,6 +26,6 @@ public final class R {
         public static final int app_name=0x7f040000;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- dicokinande/app/src/main/java/io/github/sergemastaki/dicokinande/MainActivity.java
+++ dicokinande/app/src/main/java/io/github/sergemastaki/dicokinande/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.sergemastaki.dicokinande;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -9,6 +10,14 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+		setContentView(R.layout.main);
+		
+		// Add spinner for choosing translation language
+		Spinner spinner = (Spinner) findViewById(R.id.mainSpinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+												R.array.trans_lang,
+												android.R.layout.simple_spinner_item);									
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
     }
 }
